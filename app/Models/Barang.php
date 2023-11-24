0669ca9d2e96422fb5adf35a6d96b92cefd8d8ce
RInf0 <c14h3123@gmail.com>
2023-11-24 15:21:26 +0700

    first commit

---
+++ app/Models/Barang.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Barang extends Model
+{
+    use HasFactory;
+    //pilih salah satu antara fillable atau guarded
+    protected $fillable = ['nama', 'deskripsi', 'stok'];
+    //protected $guarded = [];
+}
